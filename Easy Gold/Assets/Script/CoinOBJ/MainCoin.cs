8c90faad724b0e81812d954215f4e486187927b0
Carlo Baron <carlobaron430@gmail.com>
2023-01-24 17:14:07 +0800

    Test Revert

---
+++ Easy Gold/Assets/Script/CoinOBJ/MainCoin.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCoin : MonoBehaviour
+{
+    public Collider2D collider;
+    public Animator anim;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
